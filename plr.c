decffdea4d4301cad06549a1c45a597acd795e10
Ilias Kasoumis <ilias.kasoumis@gmail.com>
2019-04-20 20:25:55 +0100

    Revise window function code for tuple arguments in line with recent changes to non-tuple args

--- plr.c
+++ plr.c
@@ -1602,7 +1602,7 @@ plr_convertargs(plr_function *function, Datum *arg, bool *argnull, FunctionCallI
 			else if (function->arg_is_rel[i])
 			{
 				/* for tuple args, convert to a one row data.frame */
-				CONVERT_TUPLE_TO_DATAFRAME(arg[i]);
+				CONVERT_TUPLE_TO_DATAFRAME(GET_ARG_VALUE(i));
 			}
 			else if (function->arg_elem[i] == InvalidOid)
 			{
@@ -1684,64 +1684,14 @@ plr_convertargs(plr_function *function, Datum *arg, bool *argnull, FunctionCallI
 
 		if (plr_is_unbound_frame(winobj))
 		{
-
-/*<<<<<<< HEAD
-			WinGetFrameData(winobj, i, dvalues, isnulls, &numels, &has_nulls);
-            
-            if (!function->arg_is_rel[i])
-            {
-                datum_typid = function->arg_typid[i];
-                datum_out_func = function->arg_out_func[i];
-                datum_typbyval = function->arg_typbyval[i];
-                PROTECT(el = pg_datum_array_get_r(dvalues, isnulls, numels, has_nulls,
-											  datum_typid, datum_out_func, datum_typbyval));
-
-            }
-            else
-            {
-                * for tuple args, convert to a multi-row data.frame *
-                int          tpl;
-                int32        tupTypmod;
-                TupleDesc    tupdesc;
-                HeapTupleHeader tuple_hdr;
-                HeapTupleData  *tupledata = palloc0(numels * sizeof(HeapTupleData));
-                HeapTuple      *tuples = palloc0(numels * sizeof(HeapTuple));
-                for (tpl = 0; tpl < numels; tpl++)
-                {
-                    tuple_hdr = DatumGetHeapTupleHeader(dvalues[tpl]);
-                    tupledata[tpl].t_len = HeapTupleHeaderGetDatumLength(tuple_hdr);
-                    ItemPointerSetInvalid(&tupledata[tpl].t_self);
-                    tupledata[tpl].t_tableOid = InvalidOid;
-                    tupledata[tpl].t_data = tuple_hdr;
-                    tuples[tpl] = &tupledata[tpl];
-                }
-                * Get tupdesc from first tuple **assuming all are the same type** *
-                tuple_hdr = DatumGetHeapTupleHeader(dvalues[0]);
-                datum_typid = HeapTupleHeaderGetTypeId(tuple_hdr);
-                tupTypmod = HeapTupleHeaderGetTypMod(tuple_hdr);
-                tupdesc = lookup_rowtype_tupdesc(datum_typid, tupTypmod);
-                PROTECT(el = pg_tuple_get_r_frame(numels, tuples, tupdesc));
-                ReleaseTupleDesc(tupdesc);
-                pfree(tuples);
-                pfree(tupledata);
-            }
-			 *
-			 * We already set function->nargs arguments
-			 * so we must start with a function->nargs
-			 *
-			SETCAR(t, el);
-			t = CDR(t);
-
-			UNPROTECT(1);
-=======*/
 			SEXP lst;
 			if (0 == current_row)
 			{
 				lst = PROTECT(allocVector(VECSXP, function->nargs));
 				for (i = 0; i < function->nargs; i++, t = CDR(t))
 				{
-					el = get_fn_expr_arg_stable(fcinfo->flinfo, i) ?
-						R_NilValue : pg_window_frame_get_r(winobj, i, function);
+					el = /* get_fn_expr_arg_stable(fcinfo->flinfo, i) ?
+                        R_NilValue : */ (function->arg_is_rel[i] ? pg_window_frame_get_r_frame : pg_window_frame_get_r)(winobj, i, function);
 					SET_VECTOR_ELT(lst, i, el);
 					SETCAR(t, el);
 				}
@@ -1759,16 +1709,17 @@ plr_convertargs(plr_function *function, Datum *arg, bool *argnull, FunctionCallI
 					SETCAR(t, el);
 				}
 			}
-/*>>>>>>> 6496bfa431edb2d0a26b494a9de88790bb32844f*/
 		}
 		else
 			for (i = 0; i < function->nargs; i++, t = CDR(t))
 			{
-				el = get_fn_expr_arg_stable(fcinfo->flinfo, i) ?
-					R_NilValue : pg_window_frame_get_r(winobj, i, function);
-				SETCAR(t, el);
-			}
+				el = /* get_fn_expr_arg_stable(fcinfo->flinfo, i) ?
+					R_NilValue : */ (function->arg_is_rel[i] ? pg_window_frame_get_r_frame : pg_window_frame_get_r)(winobj, i, function);
 
+                SETCAR(t, el);
+			}
+        
+        // below only works if last el <> R_NilValue (see commented out above)
 		numels = function->nargs > 0 ? GET_LENGTH(el) : 0;
 
 		/* fnumrows */
